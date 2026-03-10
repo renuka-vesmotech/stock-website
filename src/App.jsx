f4ae24019f86b83de5ea2658bc449234942994f2
renuka-vesmotech <renuka@vesmotech.com>
2026-03-10 14:42:24 +0530

    new branch changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import Contact from "./Contact";
 import Login from "./Login";
 import Signup from "./Signup";
 
